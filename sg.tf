e3a437af7b60f84b18631cc2254e84d727054eb5
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2024-03-13 12:08:47 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "allow_tls" {
-  name   = var.sgname            # define this variable
-  vpc_id = var.vpc_id           # define this variable
+  name   = var.sgname # define this variable
+  vpc_id = var.vpc_id # define this variable
 
   # Ingress rules
   dynamic "ingress" {
-    for_each = var.port           # define this variable
+    for_each = var.port # define this variable
     content {
       description = "TLS from VPC"
-      from_port   = ingress.value   #ingress.value-gave value from (ingress) meaning dynamic block name and (value) taken from for_each = var.port   
+      from_port   = ingress.value #ingress.value-gave value from (ingress) meaning dynamic block name and (value) taken from for_each = var.port   
       to_port     = ingress.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
@@ -23,11 +23,24 @@ resource "aws_security_group" "allow_tls" {
   }
 
   tags = {
-    Name = "var.sg-tag"              # define this variable
+    Name = var.sg-tag # define this variable
   }
 }
 
-var.sg-tag{}   
-var.sgname{}    
-var.vpc_id{} 
-var.port{}
+
+
+#######################################variables
+variable "sg-tag" {}
+variable "sgname" {}
+variable "vpc_id" {}
+variable "port" {
+  type    = list(number)
+  default = [443, 80, 8080] # Example values, adjust as needed.
+}
+#################################################
+######create new terraform.tfvars for below content
+sg-tag = "Name=my-sg"
+sgname = "sg"
+vpc_id = "vpc-066da1176533784ff"
+
+
